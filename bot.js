34c4c30380eae87612aedb8c3f9e4159d350ef01
TmrBotz <tmrbotz@gmail.com>
2025-04-14 22:08:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ bot.on('callback_query', (query) => {
       message_id: query.message.message_id
     });
 
-    const caption = `*${state.name}*\n\nLanguage: ${state.language}\nQuality: ${state.quality}`;
+    const caption = `<b><a href="t.me/Sky_Hub4u">#ɴᴇᴡ_ғɪʟᴇ_ᴀᴅᴅᴇᴅ ✅</a></b>\n\n<b>🔰Nᴀᴍᴇ:</b> <code>${state.name}</code>\n<b>✨Aᴜᴅɪᴏ: ${state.language}\n♻️Qᴜᴀʟɪᴛʏ: ${state.quality}</b>`;
 
     // Send to user (admin)
     bot.sendPhoto(chatId, state.poster, {
